28bbc7400f07c3374f77e29f494bcf3074ca9e35
Maggie Saperstein <maggie.saperstein@gmail.com>
2022-07-01 16:26:24 -0500

    commit tree 198b4aba41deada082ee898b97f857b50c5fc6fd

--- lib/person.rb
+++ lib/person.rb
@@ -1,3 +1,20 @@
 class Person
-
+  #getter
+  #getter and setter methods are how to fill out the objects in ruby
+  #make it force of habit to automatically fill in the end when doing anything
+  def name=(name)
+    @name=name
+  end
+  #setter
+  def name
+    @name
+  end
+  #getter
+  def job=(job)
+    @job=job
+  end
+  #setter
+  def job
+    @job
+  end
 end
